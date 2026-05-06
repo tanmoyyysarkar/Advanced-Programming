b59fe89ed739f269451fb62512b5377109b5754e
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2026-05-06 21:28:00 +0530

    Order processor interface added

---
+++ Assignment 12/order/OrderProcessor.java
@@ -0,0 +1,9 @@
+package order;
+
+import model.Order;
+
+// ISP: Only order-processing behaviour — small and focused
+public interface OrderProcessor {
+    Order createOrder(String orderId, String customerName, String customerEmail, String customerPhone, double baseAmount);
+    String getOrderType();
+}
